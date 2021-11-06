41b44df1140487713da0c6c26c47167f4abecefe
Ruslan <shaggy@Hawking.selectel.ru>
2021-11-07 01:15:03 +0300

    finish navbar styling

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,10 +7,15 @@ export default class NavBar extends Component {
 
           <nav className="navbar">
 
-            <div className="home-border"></div>
-            <div className="poke-dex-border"></div>
-            <div className="poke-list-border"></div>
-
+            <div className="home-border">
+              <a className="navbar-home" href="#">Home</a>
+            </div>
+            <div className="poke-dex-border">
+              <a className="axel-poke-dex" href="#">Axel Poke Dex</a>
+            </div>
+            <div className="poke-list-border">
+              <a className="axel-poke-list" href="#">Axel Poke List</a>
+            </div>
             <form className="search-border">
               <input className="search" type="text" placeholder="Search" />
             </form>
