02f005295c671e7d1957942a4a8b8ae2775f9727
Ruslan <shaggy@Hawking.selectel.ru>
2021-11-07 14:44:28 +0300

    add pokemon-card background image and pokemon list padding

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -7,7 +7,7 @@ export default class PokemonList extends Component {
             <div>
 
             <div class="album py-5 bg-light">
-            <div class="container">
+            <div class="container pokemon-list">
             <div class="row">
                 
                 <div class="col-md-4">
@@ -34,6 +34,17 @@ export default class PokemonList extends Component {
                 </div>
                 </div>
 
+                <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <PokemonCard/>
+                </div>
+                </div>
+
+                <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                    <PokemonCard/>
+                </div>
+                </div>
             </div>
             </div>
             </div>
