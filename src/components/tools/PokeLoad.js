2d8557921396806a3dcbed87085280a627298669
Ruslan <kornerus@fjfi.cvut.cz>
2021-11-15 13:01:13 +0300

    add PokeLoad components

---
+++ src/components/tools/PokeLoad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class PokeLoad extends Component {
+    render() {
+        return (
+            <div class="center-on-page">
+                <div class="pokeball">
+                    <div class="pokeball__button"></div>
+                </div>
+            </div>
+        )
+    }
+}
